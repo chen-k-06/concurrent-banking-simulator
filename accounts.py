c03be57de4ee69bbc33f649c5280a3840cf252db
Kexin <kkc2160@columbia.edu>
2026-01-12 07:24:57 +0000

    Moved account class to accounts.py

--- accounts.py
+++ accounts.py
@@ -1,5 +1,23 @@
-from account import Account
+from threading import Lock
 
+class Account:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+        self.lock = Lock()
+
+def deposit(user:Account, amount: float):
+    with user.lock:
+        user.balance += amount
+        return True    
+        
+def withdraw(user:Account, amount: float):
+    with user.lock: 
+        if user.balance >= amount:
+            user.balance -= amount 
+            return True
+        return False
+    
 def transfer(sender: Account, receiver: Account, amount: float):
     # acquire locks in consistent order to prevent deadlocks 
     first, second = (sender, receiver) if sender.owner > receiver.owner else (receiver, sender)
