0a6bb374d260b9889969723f8a9774333a341ab9
Kexin <kkc2160@columbia.edu>
2026-01-12 04:10:35 +0000

    Account class completed

--- account.py
+++ account.py
@@ -1,4 +1,19 @@
+from threading import Lock
+
 class Account:
     def __init__(self, owner, balance=0):
         self.owner = owner
         self.balance = balance
+        self.lock = Lock()
+
+    def deposit(self, amount: float):
+        with self.lock:
+            self.balance += amount
+            return True    
+        
+    def withdraw(self, amount: float):
+        with self.lock: 
+            if self.balance >= amount:
+                self.balance -= amount 
+                return True
+            return False
